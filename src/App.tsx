243af8554b8764ad7908eb0d853ff00a12944131
Aleksandr <badaev92aleksandr@gmail.com>
2024-12-03 00:05:33 +0100

    Add Lesson 08

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import "./App.css";
-
+import GlobalStyles from "./styles/GlobalStyles";
 
 //lessons
 // import Lesson06 from "./lessons/Lesson06/Lesson06";
 // import Lesson07 from "./lessons/Lesson07/Lesson07";
+import Lesson08 from "./lessons/Lesson08/Lesson08";
 
 //homeworks
-import Homework07 from "./homeworks/Homework07/Homework07";
+// import Homework07 from "./homeworks/Homework07/Homework07";
 
 //consultations
 
 function App() {
-  return(
-  <div className="App">
+  return (
+    <>
+      <GlobalStyles />
       {/* <Lesson06 /> */}
       {/* <Lesson07 /> */}
-      <Homework07/>
-
-  </div>
+      {/* <Homework07/> */}
+      <Lesson08 />
+    </>
   );
 }
 
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,15 +1,18 @@
 import Button from "../Button/Button";
 import { CounterContainer, CounterResult, ButtonWrapper } from "./styles";
+import { CounterProps } from "./types";
 //Шаг 1 - импорт хука useState из react
-import { useState } from "react";
+//import { useState } from "react";
 
-function Counter() {
+function Counter({countValue, onMinus, onPlus}:CounterProps) {
   console.log("Render");
 
   //Шаг 2 - вызываем хук useState и передаем в него первоначальное состояние (InitialState)
   //Вызываем деструктуризацию массива из двух элементов, который возвращает хук useState
   // const [count, setCount] = useState<number | undefined>(0);
-  const [count, setCount] = useState<number>(0);
+
+  // const [count, setCount] = useState<number>(0); <-----------------
+
   //   const result = useState(0);
   //   console.log(result);
   //   const count = result[0];
@@ -17,22 +20,22 @@ function Counter() {
   //   const setCount = result[1];
   //   console.log(setCount);
 
-  const onPlusClick = (): void => {
-    setCount((prevValue) => prevValue + 1);
-  };
+  // const onPlusClick = (): void => {
+  //   setCount((prevValue) => prevValue + 1);
+  // };
 
-  const onMinusClick = (): void => {
-    setCount((prevValue) => prevValue - 1);
-  };
+  // const onMinusClick = (): void => {
+  //   setCount((prevValue) => prevValue - 1);
+  // };
 
   return (
     <CounterContainer>
       <ButtonWrapper>
-        <Button name="-" onClick={onMinusClick} />
+        <Button name="-" onClick={onMinus} />
       </ButtonWrapper>
-      <CounterResult>{count}</CounterResult>
+      <CounterResult>{countValue}</CounterResult>
       <ButtonWrapper>
-        <Button name="+" onClick={onPlusClick} />
+        <Button name="+" onClick={onPlus} />
       </ButtonWrapper>
     </CounterContainer>
   );
