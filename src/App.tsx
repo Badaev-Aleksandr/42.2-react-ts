ca791a8bde6c2af72f7ce5c8e50b6d02953a307a
Aleksandr <badaev92aleksandr@gmail.com>
2024-12-10 18:31:17 +0100

    Add Lesson10

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+
 import GlobalStyles from "./styles/GlobalStyles";
 
 //lessons
 // import Lesson06 from "./lessons/Lesson06/Lesson06";
 // import Lesson07 from "./lessons/Lesson07/Lesson07";
 // import Lesson08 from "./lessons/Lesson08/Lesson08";
-import Lesson09 from "lessons/Lesson09/Lesson09";
+// import Lesson09 from "lessons/Lesson09/Lesson09";
+
 
 //homeworks
 // import Homework07 from "./homeworks/Homework07/Homework07";
@@ -21,7 +23,7 @@ function App() {
       {/* <Homework07/> */}
       {/* <Lesson08 /> */}
       {/* <Homework08 /> */}
-      <Lesson09 />
+      {/* <Lesson09 /> */}
     </>
   );
 }
