10501033c37176f7e09723b7f3aedef30d6836e3
Aleksandr <badaev92aleksandr@gmail.com>
2024-12-15 23:20:39 +0100

    Create: Homework 10

--- src/lessons/Lesson10/Lesson10/Lesson10.tsx
+++ src/lessons/Lesson10/Lesson10/Lesson10.tsx
@@ -1,43 +1,72 @@
 import Button from "components/Button/Button";
+
 import {
+  ButtonLi,
   ButtonWrapper,
   Lesson10Container,
   ResultLi,
   ResultList,
   ResultWrapper,
+  WrapperElement,
 } from "./styles";
+
 import { useEffect, useState } from "react";
+
 import axios from "axios";
+import { v4 } from "uuid";
 
 function Lesson10() {
-  const [catInfo, setCatInfo] = useState<string>("");
+  const [facts, setFacts] = useState<string[]>([]);
 
   const CAT_INFO_URL = " https://catfact.ninja/fact";
+
+  const deleteFact = (index: number) => {
+    setFacts((prevFacts) => prevFacts.filter((_, i) => i !== index));
+  };
+
+  const resultCatsInfo = facts.map((fact, index) => {
+    return (
+      <WrapperElement key={v4()}>
+        <ResultLi>{fact}</ResultLi>
+        <ButtonLi>
+          <Button name="Delete" onClick={() => deleteFact(index)} />
+        </ButtonLi>
+      </WrapperElement>
+    );
+  });
 
   const fetchDate = async () => {
     try {
       const respons = await axios.get(CAT_INFO_URL);
-      setCatInfo(respons.data.fact)
+      //   setCatInfo(respons.data.fact)
+      setFacts((arrayInfo) => [...arrayInfo, respons.data.fact]);
       console.log(respons);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const getMoreInfo = () => {
+    fetchDate();
+  };
+
+  const allDelete = () => {
+    setFacts([]);
+  };
+
   useEffect(() => {
     fetchDate();
   }, []);
+  console.log(facts);
 
   return (
     <Lesson10Container>
       <ButtonWrapper>
-        <Button name="GET MORE INFO" />
-        <Button name="DELETE ALL DATA" />
+        <Button name="GET MORE INFO" onClick={getMoreInfo} />
+        <Button name="DELETE ALL DATA" onClick={allDelete} />
       </ButtonWrapper>
       <ResultWrapper>
-        <ResultList>
-          <ResultLi></ResultLi>
-        </ResultList>
+        <ResultList>{resultCatsInfo}</ResultList>
       </ResultWrapper>
     </Lesson10Container>
   );
--- src/lessons/Lesson10/Lesson10/styles.ts
+++ src/lessons/Lesson10/Lesson10/styles.ts
@@ -1,7 +1,53 @@
 import styled from "@emotion/styled";
 
-export const Lesson10Container = styled.div``
-export const ButtonWrapper = styled.div``
-export const ResultWrapper = styled.div``
-export const ResultList = styled.ul``
-export const ResultLi = styled.li``
+export const Lesson10Container = styled.div`
+  display: flex;
+  flex: 1;
+  padding: 20px;
+  gap: 20px;
+`;
+
+export const WrapperElement = styled.div`
+display: flex;
+justify-content: space-between;
+gap: 10px;
+`
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  width: 350px;
+  height: 180px;
+  gap: 15px;
+`;
+export const ButtonLi = styled.div`
+  width: 150px;
+`;
+
+export const ResultWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  height: 700px;
+  width: 800px;
+  overflow: auto;
+  border: 1px solid black;
+`;
+export const ResultList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const ResultLi = styled.li`
+  display: flex;
+  font-size: 20px;
+  width: 550px;
+  height: 68px;
+  overflow: auto;
+  padding: 10px;
+  border: 1px solid blue;
+  border-radius: 4px;
+  
+`;
